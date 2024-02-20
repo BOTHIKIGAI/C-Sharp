40b4017dbc372b3247e03442990d4c6820a8a7e8
Juan Esteban Cajiao Madero <estebax89@gmail.com>
2024-02-19 21:27:06 -0500

    Tercer dia de aprendizaje

--- 0_Introduction/MouroDev/obj/Debug/net8.0/0_Introduction.AssemblyInfo.cs
+++ 0_Introduction/MouroDev/obj/Debug/net8.0/0_Introduction.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("0_Introduction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7d044b43c86e4bd74c373cd873dc9737abe1443")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0f30cb1f2048d223823509cda7cab559fa75b6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("0_Introduction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("0_Introduction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
